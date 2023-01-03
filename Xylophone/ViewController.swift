d9a0c501c7e242d9c8a1fa450e47d8ce62617385
rabbikim <boykhk98@naver.com>
2023-01-03 23:08:42 +0900

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation // AudioVisual Foundation module
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        var buttonTitle = sender.currentTitle
+        playSound(soundName: buttonTitle!)
+        
+        sender.alpha = 0.5 // 불투명도 0.5 설정
+        
+        // 0.2초 뒤에 불투명도 1.0 설정
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
